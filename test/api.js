14030d8aa08edb1417e197a3bb6e4dc2f7a95b75
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-11-01 19:30:00 +0100

    Update lib: Impl .rel(), .get(), .has()
    Also, fix some tests

---
+++ test/api.js
@@ -0,0 +1,31 @@
+var assert = require( 'assert' )
+var inspect = require( './inspect' )
+var Link = require( '..' )
+
+suite( 'API', function() {
+
+  test( 'get("rel", "next")', function() {
+    var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
+    // console.log( link.get( 'rel', 'next' ) )
+    assert.deepEqual( link.get( 'rel', 'next' ), {
+      uri: 'https://acme-staging.api.letsencrypt.org/acme/new-authz',
+      rel: 'next'
+    })
+  })
+
+  test( 'rel("next")', function() {
+    var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
+    // console.log( link.ret( 'next' ) )
+    assert.deepEqual( link.rel( 'next' ), {
+      uri: 'https://acme-staging.api.letsencrypt.org/acme/new-authz',
+      rel: 'next'
+    })
+  })
+
+  test( 'has("rel", "next")', function() {
+    var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
+    // console.log( link.has( 'rel', 'next' ) )
+    assert.deepEqual( link.has( 'next' ), true )
+  })
+
+})
