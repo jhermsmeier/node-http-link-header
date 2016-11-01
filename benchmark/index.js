b5878fa9d4aeb63744a13e0ef7953bfff1603fbb
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-11-01 18:48:19 +0100

    Add benchmark

---
+++ benchmark/index.js
@@ -0,0 +1,9 @@
+var Link = require( '..' )
+
+suite( 'http-link-header', function() {
+
+  bench( 'parse', function() {
+    return Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
+  })
+
+})
