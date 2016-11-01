0bd938d3f6e0b7c78a5934d07442827a9ab2ae98
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-11-02 00:12:07 +0100

    Update lib: Impl .set()

--- test/api.js
+++ test/api.js
@@ -6,7 +6,7 @@ suite( 'API', function() {
 
   test( 'get("rel", "next")', function() {
     var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
-    // console.log( link.get( 'rel', 'next' ) )
+    // console.log( inspect( link.get( 'rel', 'next' ) ) )
     assert.deepEqual( link.get( 'rel', 'next' ), {
       uri: 'https://acme-staging.api.letsencrypt.org/acme/new-authz',
       rel: 'next'
@@ -15,7 +15,7 @@ suite( 'API', function() {
 
   test( 'rel("next")', function() {
     var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
-    // console.log( link.ret( 'next' ) )
+    // console.log( inspect( link.ret( 'next' ) ) )
     assert.deepEqual( link.rel( 'next' ), {
       uri: 'https://acme-staging.api.letsencrypt.org/acme/new-authz',
       rel: 'next'
@@ -24,7 +24,7 @@ suite( 'API', function() {
 
   test( 'has("rel", "next")', function() {
     var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
-    // console.log( link.has( 'rel', 'next' ) )
+    // console.log( inspect( link.has( 'rel', 'next' ) ) )
     assert.deepEqual( link.has( 'next' ), true )
   })
 
@@ -32,6 +32,34 @@ suite( 'API', function() {
     var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
     var expected = '<https://acme-staging.api.letsencrypt.org/acme/new-authz>; rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>; rel="terms-of-service"'
     assert.equal( link.toString(), expected )
+  })
+
+  test( 'set( add )', function() {
+    var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
+    link.set({
+      uri: 'https://github.com/ietf-wg-acme/acme/',
+      rel: 'example'
+    })
+    // console.log( inspect( link ) )
+    assert.equal( link.refs.length, 3 )
+    assert.deepEqual( link.get( 'rel', 'example' ), {
+      uri: 'https://github.com/ietf-wg-acme/acme/',
+      rel: 'example'
+    })
+  })
+
+  test( 'set( overwrite )', function() {
+    var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
+    link.set({
+      uri: 'https://not-the-same.com/',
+      rel: 'next'
+    })
+    // console.log( inspect( link ) )
+    assert.equal( link.refs.length, 2 )
+    assert.deepEqual( link.get( 'rel', 'next' ), {
+      uri: 'https://not-the-same.com/',
+      rel: 'next'
+    })
   })
 
 })
