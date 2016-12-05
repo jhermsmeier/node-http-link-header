e844ea4499c31be69aac1f3b17e4302fe61e2c71
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-12-05 18:19:06 +0100

    Fix misinterpr. of multiple "rel" attrs rule in RFC
    Ref #7

--- test/api.js
+++ test/api.js
@@ -7,7 +7,7 @@ suite( 'API', function() {
   test( 'get("rel", "next")', function() {
     var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
     // console.log( inspect( link.get( 'rel', 'next' ) ) )
-    assert.deepEqual( link.get( 'rel', 'next' ), {
+    assert.deepEqual( link.get( 'rel', 'next' )[0], {
       uri: 'https://acme-staging.api.letsencrypt.org/acme/new-authz',
       rel: 'next'
     })
@@ -16,7 +16,7 @@ suite( 'API', function() {
   test( 'rel("next")', function() {
     var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
     // console.log( inspect( link.ret( 'next' ) ) )
-    assert.deepEqual( link.rel( 'next' ), {
+    assert.deepEqual( link.rel( 'next' )[0], {
       uri: 'https://acme-staging.api.letsencrypt.org/acme/new-authz',
       rel: 'next'
     })
@@ -34,7 +34,7 @@ suite( 'API', function() {
     assert.equal( link.toString(), expected )
   })
 
-  test( 'set( add )', function() {
+  test( 'set()', function() {
     var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
     link.set({
       uri: 'https://github.com/ietf-wg-acme/acme/',
@@ -42,24 +42,69 @@ suite( 'API', function() {
     })
     // console.log( inspect( link ) )
     assert.equal( link.refs.length, 3 )
-    assert.deepEqual( link.get( 'rel', 'example' ), {
+    assert.deepEqual( link.get( 'rel', 'example' )[0], {
       uri: 'https://github.com/ietf-wg-acme/acme/',
       rel: 'example'
     })
   })
 
-  test( 'set( overwrite )', function() {
+  test( 'set()', function() {
     var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
     link.set({
       uri: 'https://not-the-same.com/',
       rel: 'next'
     })
     // console.log( inspect( link ) )
-    assert.equal( link.refs.length, 2 )
-    assert.deepEqual( link.get( 'rel', 'next' ), {
+    assert.equal( link.refs.length, 3 )
+    assert.equal( link.get( 'rel', 'next' ).length, 2 )
+    assert.deepEqual( link.get( 'rel', 'next' )[1], {
       uri: 'https://not-the-same.com/',
       rel: 'next'
     })
+  })
+
+  test( 'set()', function() {
+    var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
+    link.set({
+      uri: 'https://not-the-same.com/',
+      rel: 'next'
+    })
+    // console.log( inspect( link ) )
+    assert.equal( link.refs.length, 3 )
+    assert.deepEqual( link.get( 'rel', 'next' )[0], {
+      uri: 'https://acme-staging.api.letsencrypt.org/acme/new-authz',
+      rel: 'next'
+    })
+  })
+
+  test( 'parse() multiple', function() {
+
+    var links = new Link()
+
+    links.parse( '<example.com>; rel="example"; title="Example Website"' )
+    assert.deepEqual( links, {
+      refs: [
+        { uri: 'example.com', rel: 'example', title: 'Example Website' },
+      ]
+    })
+
+    links.parse( '<example-01.com>; rel="alternate"; title="Alternate Example Domain"' )
+    assert.deepEqual( links, {
+      refs: [
+        { uri: 'example.com', rel: 'example', title: 'Example Website' },
+        { uri: 'example-01.com', rel: 'alternate', title: 'Alternate Example Domain' },
+      ]
+    })
+
+    links.parse( '<example-02.com>; rel="alternate"; title="Second Alternate Example Domain"' )
+    assert.deepEqual( links, {
+      refs: [
+        { uri: 'example.com', rel: 'example', title: 'Example Website' },
+        { uri: 'example-01.com', rel: 'alternate', title: 'Alternate Example Domain' },
+        { uri: 'example-02.com', rel: 'alternate', title: 'Second Alternate Example Domain' },
+      ]
+    })
+
   })
 
 })
