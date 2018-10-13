4b811cfab73e3644981556a7859e36978a192567
Jonas Hermsmeier <jhermsmeier@gmail.com>
2018-10-13 13:46:55 +0200

    test(inspect): Add inspect.log()

--- test/inspect.js
+++ test/inspect.js
@@ -1,9 +1,13 @@
 var util = require( 'util' )
-var options = {
-  depth: null,
-  colors: true,
-}
+var options = { depth: null, colors: process.stdout.isTTY }
 
-module.exports = function inspect( value ) {
+function inspect( value ) {
   return util.inspect( value, options )
 }
+
+inspect.log = function( value ) {
+  process.stdout.write( inspect( value ) )
+  process.stdout.write( '\n' )
+}
+
+module.exports = inspect
