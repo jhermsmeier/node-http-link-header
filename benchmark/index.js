01dac040dbd275ea5676b0829a1ef1ea4ff94357
Jonas Hermsmeier <jhermsmeier@gmail.com>
2018-10-13 13:39:41 +0200

    refactor(benchmark): Replace matcha with nanobench

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,15 +1,30 @@
+var bench = require( 'nanobench' )
 var Link = require( '..' )
 
-suite( 'http-link-header', function() {
+var ITERATIONS = 100000
 
-  bench( 'parse', function() {
-    return Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
-  })
+bench( `http-link-header .parse() ⨉ ${ITERATIONS}`, function( run ) {
 
-  var link = Link.parse( '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"' )
+  var value = '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"'
+  var link = null
 
-  bench( 'toString', function() {
-    return link.toString()
-  })
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    link = Link.parse( value )
+  }
+  run.end()
+
+})
+
+bench( `http-link-header #toString() ⨉ ${ITERATIONS}`, function( run ) {
+
+  var value = '<https://acme-staging.api.letsencrypt.org/acme/new-authz>;rel="next", <https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf>;rel="terms-of-service"'
+  var link = Link.parse( value )
+
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    value = link.toString()
+  }
+  run.end()
 
 })
